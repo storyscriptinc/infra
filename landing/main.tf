7eeec0d51c8401d37d7df2dc3e0151f91097715b
Will Martin <will@storyscript.io>
2020-01-14 18:11:06 +0100

    Add TF scripts for storyscript.com DNS records

---
+++ landing/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  # The modules used in this example have been updated with 0.12 syntax, additionally we depend on a bug fixed in
+  # version 0.12.7.
+  required_version = ">= 0.12.7"
+  backend "gcs" {
+    bucket = "storyscript-infra"
+    prefix  = "landing/state"
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# PREPARE PROVIDERS
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "google" {
+  credentials = var.credentials
+  version = "~> 2.9.0"
+  project = "storyscript"
+  region  = "us-east4"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE DNS RECORDS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_dns_record_set" "storyscript-apex" {
+  name = "storyscript.com."
+  type = "A"
+  ttl = 300
+
+  managed_zone = "storyscript-com"
+  rrdatas = [ "104.198.14.52" ]
+}
+
+
+resource "google_dns_record_set" "storyscript-www" {
+  name = "www.storyscript.com."
+  type = "CNAME"
+  ttl = 300
+
+  managed_zone = "storyscript-com"
+  rrdatas = [ "storyscript.com." ]
+}
+
+resource "google_dns_record_set" "storyscript-wildcard" {
+  name = "*.storyscript.com."
+  type = "CNAME"
+  ttl = 300
+
+  managed_zone = "storyscript-com"
+  rrdatas = [ "storyscript.io." ]
+}
