4965b4ce384320f3ee61363d8a4f6b1820b5fde4
Will Martin <will@storyscript.io>
2021-07-19 13:10:25 +0200

    Enable http to https redirect for app
    Signed-off-by: Ahmad Sattar Atta <ahmad@story.ai>

--- app/main.tf
+++ app/main.tf
@@ -56,6 +56,32 @@ resource "google_compute_target_https_proxy" "default" {
 }
 
 # ------------------------------------------------------------------------------
+# HTTP to HTTPS redirect using 301s
+# ------------------------------------------------------------------------------
+
+resource "google_compute_url_map" "http-redirect" {
+  name = "http-redirect"
+
+  default_url_redirect {
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"  // 301 redirect
+    strip_query            = false
+    https_redirect         = true  // this is the magic
+  }
+}
+
+resource "google_compute_target_http_proxy" "http-redirect" {
+  name    = "http-redirect"
+  url_map = google_compute_url_map.http-redirect.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "http-redirect" {
+  name       = "http-redirect"
+  target     = google_compute_target_http_proxy.http-redirect.self_link
+  ip_address = google_compute_global_address.default.address
+  port_range = "80"
+}
+
+# ------------------------------------------------------------------------------
 # CREATE A RECORD POINTING TO THE PUBLIC IP OF THE CLB
 # ------------------------------------------------------------------------------
 
